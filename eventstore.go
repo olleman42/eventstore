2cad04757b0fce7dd92318b1fc7bb64ee00d8518
Sergey Popov <olleman@gmail.com>
2017-04-23 21:35:36 +0200

    Add server wrapper for event store

--- eventstore.go
+++ eventstore.go
@@ -208,23 +208,36 @@ func (e *EventStore) DumpEvents(w io.Writer) error {
 func (e *EventStore) GetAggregateHistory(aggType, aggID string, w io.Writer) error {
 	return e.Connection.View(func(tx *bolt.Tx) error {
 		tb := tx.Bucket([]byte(aggType))
+
+		if tb == nil {
+			return errors.New("Type does not exist in store: " + aggType)
+		}
+
 		ab := tb.Bucket([]byte(aggID))
+
+		if ab == nil {
+			return errors.New("Aggregate does not exist: " + aggID)
+		}
 
 		return dumpBucket(ab, w)
 	})
 }
 
 // GetTypeHistory returns a byte stream of timestamp ordered aggregate event of a certain type
-func (e *EventStore) GetTypeHistory(aggType, aggID string, w io.Writer) error {
+func (e *EventStore) GetTypeHistory(aggType string, w io.Writer) error {
 	return e.Connection.View(func(tx *bolt.Tx) error {
 		tb := tx.Bucket([]byte(aggType))
+
+		if tb == nil {
+			return errors.New("Type does not exist in store: " + aggType)
+		}
 
 		return dumpBucket(tb, w)
 	})
 }
 
 // GetHistory return a byte stream of all the events in the system ordered by timestamp
-func (e *EventStore) GetHistory(aggType, aggID string, w io.Writer) error {
+func (e *EventStore) GetHistory(w io.Writer) error {
 	// TODO Add functionality to also trickle out events that might have arrived since a read started, so no stray events disappear
 	return e.Connection.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("events"))
@@ -235,7 +248,8 @@ func (e *EventStore) GetHistory(aggType, aggID string, w io.Writer) error {
 
 func dumpBucket(b *bolt.Bucket, w io.Writer) error {
 	return b.ForEach(func(k, v []byte) error {
-		_, err := fmt.Fprint(w, v)
+		_, err := w.Write(v)
+		//_, err := fmt.Fprint(w, v)
 		return err
 	})
 }
